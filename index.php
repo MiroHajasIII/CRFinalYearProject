7ee454637d9236e24dff3c8f5564bc28b54c2c2d
MiroHajasIII <mirohajas81@gmail.com>
2024-01-25 22:57:41 +0000

    first commit of the project

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+$view = new stdClass();
+$view->pageTitle = 'index page';
+
+// area for implementing more logic once needed
+// logic ideas: user login logic
+
+require_once('Views/index.phtml');
