756ff4f7b100d0d26a298f387b27aad66ae7c82d
MiroHajasIII <mirohajas81@gmail.com>
2024-02-01 20:46:00 +0000

    fixed background + contact page

---
+++ contact.php
@@ -0,0 +1,8 @@
+<?php
+$view = new stdClass();
+$view->pageTitle = 'Contact Us';
+
+// area for implementing more logic once needed
+// logic ideas: user login logic
+
+require_once('Views/contact.phtml');
